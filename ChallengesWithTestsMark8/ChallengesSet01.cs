d0d523ccaabffeab8898f97d8b4b75222de84cef
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-17 19:03:14 -0500

    completed and tested set one, started set 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 return true;
             }
@@ -18,32 +18,41 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var _val = (number1 < number2) ? number1 : number2;
+            return _val;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(nameOfPerson))
+            {
+                var name = $"Hello, {nameOfPerson}!";
+                return name;
+            } else 
+            { 
+                return "Hello!";
+            }
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            var reply = "HEY!";
+            return reply;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,32 +11,54 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return Char.IsLetter(c); 
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            int elements = vals.Count();
+            if (elements % 2 == 0)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+           return (number % 2 == 0) ? true : false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return (num % 2 != 0) ? true : false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
+
+
             throw new NotImplementedException();
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+
+            int count1 = str1.Length;
+            int count2 = str2.Length;
+            if (count1 < count2)
+            {
+                return count1;
+            }
+            else 
+            {
+                return count2;
+            }
         }
 
         public int Sum(int[] numbers)
